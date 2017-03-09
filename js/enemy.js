20771cdb199518407e3165b2726ffd6882b36cfc
yuki1996 <laetitia.boucher96@live.fr>
2017-03-09 16:59:50 +0100

    Add files via upload

--- js/enemy.js
+++ js/enemy.js
@@ -1,4 +1,5 @@
 function Enemy() {
+	//ensemble des planètes de l'ennemie
 	this.getPlanet = function() {
 		var mySet = new Set();
 		mySet.add(p);
@@ -7,18 +8,25 @@ function Enemy() {
 	
 }
 
+
+
+//ajout de la planète p 
 Enemy.prototype.addPlanet() = function(p) {
 	if (p instanceOf Planet) {
 		this.getPlanet.add(p);
 	}
 }
 
+
+//Supprime la planète p
 Enemy.prototype.removePlanet() = function(p) {
 	if (p instanceOf Planet) {
 		this.getPlanet.remove(p);
 	}
 }
 
+
+// indique si p fait partie de l'ensemble des planètes de l'ennemie
 Enemy.prototype.hasPlanet() = function(p) {
 	if (p instanceOf Planet) {
 		return this.getPlanet.has(p);
@@ -26,4 +34,8 @@ Enemy.prototype.hasPlanet() = function(p) {
 	return false;
 }
 
+//s'il peut encore jouer
+Enemy.prototype.canPlay() = function() {
+	this.getSetPlanet.size() != 0;
+}
 }
--- js/player.js
+++ js/player.js
@@ -1,26 +1,40 @@
 function Player(p) {
-	this.getPlanet = function() {
+		if (p ! insteanceOf Planet) {
+		throw new Error("Player");
+	} 
+	
+	//ensemble des planètes du joueur
+	this.getSetPlanet = function() {
 		var mySet = new Set();
 		mySet.add(p);
 		return mySet;
 	}
 }
 
+//ajout de la planète p 
 Player.prototype.addPlanet() = function(p) {
 	if (p instanceOf Planet) {
-		this.getPlanet.add(p);
+		this.getSetPlanet.add(p);
 	}
 }
 
+
+//Supprime la planète p
 Player.prototype.removePlanet() = function(p) {
 	if (p instanceOf Planet) {
-		this.getPlanet.remove(p);
+		this.getSetPlanet.remove(p);
 	}
 }
 
+// indique si p fait partie de l'ensemble des planètes du joueur
 Player.prototype.hasPlanet() = function(p) {
 	if (p instanceOf Planet) {
-		return this.getPlanet.has(p);
+		return this.getSetPlanet.has(p);
 	}
 	return false;
+}
+
+//s'il peut encore jouer
+player.prototype.canPlay() = function() {
+	this.getSetPlanet.size() != 0;
 }
