2c0c6976c0ca5de7383da6135676aca4b29b6159
yuki1996 <laetitia.boucher96@live.fr>
2017-03-12 22:24:48 +0100

    Add files via upload

---
+++ game.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Space Conquest</title>
+	</head>
+
+	<body>
+		<script src="js/coord.js"></script>
+		<script src="js/planet.js"></script>
+		<script src="js/player.js"></script>
+		<script src="js/enemy.js"></script>
+		<script src="js/game.js"></script>
+		
+	</body>
+
+</html>
