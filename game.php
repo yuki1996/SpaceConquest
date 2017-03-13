e5891696c3b99ef31b42cc8ac14360d98b03e187
yuki1996 <laetitia.boucher96@live.fr>
2017-03-13 12:04:43 +0100

    Add files via upload

--- game.php
+++ game.php
@@ -4,14 +4,27 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Space Conquest</title>
-	</head>
-
-	<body>
 		<script src="js/coord.js"></script>
 		<script src="js/planet.js"></script>
 		<script src="js/player.js"></script>
 		<script src="js/enemy.js"></script>
 		<script src="js/game.js"></script>
+	</head>
+
+	<body>
+		<?php
+		$enemy = $_POST['enemy'];
+		$gelaxy = $_POST['carte'];
+		echo $enemy;
+		?>
+		<p>hello !</p>
+		<br>
+		<p>Resultat</p>
+		<script>
+
+			document.getElementById("game").innerHTML += "commence ...ou pas<br>";
+		</script>
+		<p id="game"></p>
 		
 	</body>
 
