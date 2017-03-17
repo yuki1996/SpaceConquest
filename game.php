caf45d506dc0428da95661ec6268a7104779251a
yuki1996 <laetitia.boucher96@live.fr>
2017-03-17 12:08:23 +0100

    Add files via upload

--- game.php
+++ game.php
@@ -1,31 +1,34 @@
+<?PHP
+session_start();
+	
+	if (!isset($_SESSION['player'])) {
+		$_SESSION['player'] = true;
+		$_SESSION['ENEMY'] = $_POST['enemy'];
+		$_SESSION['gelaxy'] = $_POST['carte'];
+	}
+	 
+?>
 <!DOCTYPE html>
 
 <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>Space Conquest</title>
-		<script src="js/coord.js"></script>
-		<script src="js/planet.js"></script>
-		<script src="js/player.js"></script>
-		<script src="js/enemy.js"></script>
-		<script src="js/game.js"></script>
+		<script src="./js/coord.js"></script>
+		<script src="./js/planet.js"></script>
+		<script src="./js/player.js"></script>
+		<script src="./js/enemy.js"></script>
+		<script src="./js/game.js"></script>
 	</head>
 
-	<body>
-		<?php
-		$enemy = $_POST['enemy'];
-		$gelaxy = $_POST['carte'];
-		echo $enemy;
-		?>
-		<p>hello !</p>
-		<br>
-		<p>Resultat</p>
-		<script>
-
-			document.getElementById("game").innerHTML += "commence ...ou pas<br>";
-		</script>
+	<body>		
 		<p id="game"></p>
-		
+	
+		<script>
+			var game = new Game(<?php echo $_SESSION['ENEMY'];?>,10,10);
+			alert("enemy : " + game.nbEnemy);
+			game.start();
+		</script>
 	</body>
 
 </html>
