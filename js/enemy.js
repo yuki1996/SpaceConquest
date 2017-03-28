d69ebf0f631094b1b7e94e6fade9f7de1604242c
MichelleChidi <michelle.njemanze@yahoo.com>
2017-03-28 12:52:51 +0200

    Add files via upload

--- js/enemy.js
+++ js/enemy.js
@@ -1,52 +1,79 @@
-function Enemy(c, p) {
-	if (!(c  instanceof Coord) ||! (p  instanceof Planet)) {
+function Enemy(p) {
+	if (!(p instanceof Planet)) {
 		throw new Error("Enemy");
 	} 
-	//ensemble des planètes de l'ennemie
-	this.getPlanet = function() {
-		var mySet = new Set();
-		mySet.add(p);
-		return mySet;
-	};
-	
-	
-	//Coordonnée de la planète
-	this.coord = c;
-		
+  
+  p.leader = this;
+	//ensemble des planètes de l'ennemi
+	this.getPlanets = [];
+  this.getPlanets.push(p);;
+  
+  //la flotte de l'ennemi
+  this.getFleet = [];
 }
-
-
 
 //ajout de la planète p 
 Enemy.prototype.addPlanet = function(p) {
 	if (p instanceof Planet) {
-		this.getPlanet.add(p);
+    p.leader = this;
+		this.getPlanets.add(p);
 	}
 };
-
 
 //Supprime la planète p
 Enemy.prototype.removePlanet = function(p) {
 	if (p instanceof Planet) {
-		this.getPlanet.delete(p);
+    p.leader = null;
+		this.getPlanets.pop(p);
 	}
 };
 
 
 // indique si p fait partie de l'ensemble des planètes de l'ennemie
 Enemy.prototype.hasPlanet = function(p) {
-	if (p instanceof Planet) {
-		return this.getPlanet.has(p);
+	if (!(p instanceof Planet)) {
+		return false;
 	}
-	return false;
+  // pas supporter par les versions de Internet Explorer plus anciennes que 9
+  if (this.getPlanets.indexOf(p) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 //s'il peut encore jouer
 Enemy.prototype.canPlay = function() {
-	return this.getSetPlanet.size() !== 0;
+	return this.getPlanets.length !== 0 && this.getFleet.length !== 0;
 };
 
-Enemy.prototype.getCoord = function() {
-		return this.coord;
+Enemy.prototype.attack = function(src, n, dst) {
+  if (!(src instanceof Planet) || !(dst instanceof Planet)) {
+		throw new Error("Not planets");
+	} 
+  if (n < 1) {
+      throw new Error("Empty spaceship!");
+  }
+  if (this.hasPlanet(src)) {
+    if (src.population - n <= 1) {
+      throw new Error("Operation too risky: no one would be left");
+    }
+    var ss = src.buildSpaceship(n, dst);
+    this.getFleet.push(ss);
+    //~ this.addSpaceship();
+  } else {
+    throw new Error("Not your planet");
+  }
 };
 
+Enemy.prototype.addSpaceship = function(s) {
+	if (s instanceof SpaceShip) {
+		this.getFleet.push(s);
+	}
+};
+
+Enemy.prototype.removeSpaceship = function(s) {
+	if (s instanceof Spaceship) {
+		this.getFleet.pop(s);
+	}
+};
